46ba669222b889241e5b1fabc2f2b23c02446346
heyAndrw <monoku@MacBook-Pro-de-Monoku.local>
2016-05-31 18:36:59 -0500

    fix

--- public/redesign/js/actions.js
+++ public/redesign/js/actions.js
@@ -1,3 +1,26 @@
-$(document).ready(function(){
-	$( "#branding" ).load( "pages/branding.html" );
-});
+		$(document).ready(function(){
+			$( "#branding" ).load( "pages/branding.html" );
+		});
+
+// save image
+		$(document).ready(function(){
+		var element = $("#brandinpreview"); // global variable
+		var getCanvas; // global variable
+		 
+		    $("#btn-Preview-Image").on('click', function () {
+		         html2canvas(element, {
+		         onrendered: function (canvas) {
+		                $("#previewImage").append(canvas);
+		                getCanvas = canvas;
+		             }
+		         });
+		    });
+
+			$("#btn-Convert-Html2Image").on('click', function () {
+		    var imgageData = getCanvas.toDataURL("image/png");
+		    // Now browser starts downloading it instead of just showing it
+		    var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
+		    $("#btn-Convert-Html2Image").attr("download", "heyfont_preview.png").attr("href", newData);
+			});
+
+		});
